c99d388565454d506e35a3b748949f39008bd0de
Anna Rankin <anna@annarankin.com>
2018-12-13 17:12:13 -0500

    Just what is needed

--- app.js
+++
@@ -1,5 +0,0 @@
-var express = require('express');
-var port = process.env.PORT || 3000
-var app = express().use(express.static('public')).listen(port, function() {
-  console.log("Alive on " + port)
-})
